5234d1b94918d25a61d565f8000e2f2e0ee34d66
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2025-12-25 15:16:49 +0100

    fixed migration message

--- migrations/1.1.0.lua
+++ migrations/1.1.0.lua
@@ -2,7 +2,7 @@
 -- MIGRATION SCRIPT FOR V1.1.0
 ---------------------------------------------------------------------------------------------------
 
-game.print("[color=acid]Zithorian's Extra Storage Tanks for 2.0 (Lite):[/color]")
+game.print("[color=acid]Zithorian's Extra Storage Tanks for 2.0:[/color]")
 game.print("  Regarding v1.1.0: Internal names of entities and settings were changed!")
 game.print("  If storage tank capacities were customized, it is best to exit to main menu")
 game.print("  without saving, reconfigure settings and then reload the save.")
