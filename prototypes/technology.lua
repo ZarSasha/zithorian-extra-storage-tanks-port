51c13875e87491ac080a230b964408bc11f83a27
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-08-07 18:46:22 +0200

    Update technology.lua

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -13,18 +13,18 @@ require "shared"
 local fluid_handling_tech = data.raw["technology"]["fluid-handling"]
 if fluid_handling_tech == nil then log("fluid-handling tech missing!") return end
 
-local function add(TechName, RecipeName)
-    table.insert(TechName.effects, {type = "unlock-recipe", recipe = RecipeName})
+local function add(TechName, Order, RecipeName)
+    table.insert(TechName.effects, Order, {type = "unlock-recipe", recipe = RecipeName})
 end
 
 if mods["Krastorio2"] then
     local steel_fluid_tank_tech = data.raw["technology"]["kr-steel-fluid-tanks"]
     if steel_fluid_tank_tech == nil then log("kr-steel-fluid-tanks tech missing!") return end
-    for _, variant in pairs({"1x1","2x2"}) do
-        add(fluid_handling_tech, STORAGE_TANK_NAME[variant])
+    for _, variant in pairs({"2x2","1x1"}) do -- reversed, input from start of list
+        add(fluid_handling_tech, 1, STORAGE_TANK_NAME[variant])
     end
     for _, variant in pairs({"3x4","5x5"}) do
-        add(steel_fluid_tank_tech, STORAGE_TANK_NAME[variant])
+        add(steel_fluid_tank_tech,  STORAGE_TANK_NAME[variant])
     end
 else
     for _, variant in pairs({"1x1","2x2","3x4","5x5"}) do
