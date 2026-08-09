f28a8be4913e256a04fad20bb68dd32cb0cc33cb
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-08-09 11:47:02 +0200

    revert

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -7,25 +7,23 @@ require "shared"
 ---------------------------------------------------------------------------------------------------
 -- TECHNOLOGY UNLOCK
 ---------------------------------------------------------------------------------------------------
--- Unlocks the smaller storage tanks with Fluid Handling tech, the larger ones with their own,
--- separate tech. If Krastorio 2 is installed, the later will instead be unlocked with the Steel
--- Fluid Tanks tech.
+-- Unlocks all storage tanks with Fluid Handling tech. If Krastorio 2 is installed, the larger ones
+-- will instead be unlocked with the Steel Fluid Tanks tech.
 
 local fluid_handling_tech = data.raw["technology"]["fluid-handling"]
 if fluid_handling_tech == nil then
     log("fluid-handling tech is missing!") return
 end
 
--- Adds just the small storage tanks to Fluid Handling tech.
-for _, variant in pairs({"1x1", "2x2"}) do
-    table.insert(
-        fluid_handling_tech.effects,
-        {type = "unlock-recipe", recipe = STORAGE_TANK_NAME[variant]}
-    )
-end
-
 -- Krastorio 2:
 if KRASTORIO_2.IsPresent then
+    -- Adds the small storage tanks to Fluid Handling tech.
+    for _, variant in pairs({"1x1", "2x2"}) do
+        table.insert(
+            fluid_handling_tech.effects,
+            {type = "unlock-recipe", recipe = STORAGE_TANK_NAME[variant]}
+        )
+    end
     -- Adds the large storage tanks to Steel Fluid Tanks tech.
     local steel_fluid_tank_tech = data.raw["technology"]["kr-steel-fluid-tanks"]
     if steel_fluid_tank_tech == nil then
@@ -39,27 +37,13 @@ if KRASTORIO_2.IsPresent then
     end
 -- Default:
 else
-    -- Adds the large storage tanks to their own separate tech.
-    local storageTankTech = {
-        type = "technology",
-        name = "zith-storage-tanks",
-        icon = TECH_PATH .. "zith-storage-tanks.png",
-        icon_size = 256,
-        prerequisites = {"fluid-handling"},
-        effects = {
-            { type = "unlock-recipe", recipe = STORAGE_TANK_NAME["3x4"] },
-            { type = "unlock-recipe", recipe = STORAGE_TANK_NAME["5x5"] },
-        },
-        unit = {
-            count = 25,
-            ingredients = {
-                {"automation-science-pack", 1},
-                {"logistic-science-pack", 1}
-            },
-            time = 15
-        }
-    }
-    data:extend({storageTankTech})
+    -- Adds all the storage tanks to Fluid Handling tech.
+    for _, variant in pairs({"1x1", "2x2", "3x4","5x5"}) do
+        table.insert(
+            fluid_handling_tech.effects,
+            {type = "unlock-recipe", recipe = STORAGE_TANK_NAME[variant]}
+        )
+    end
 end
 
 ---------------------------------------------------------------------------------------------------
